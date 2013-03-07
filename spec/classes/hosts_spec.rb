19988736ba50def78d62312f6c0f704beca26100
Emiliano Castagnari <ecastag@gmail.com>
2013-03-07 09:20:53 -0300

    Tests for enable_motd

--- spec/classes/hosts_spec.rb
+++ spec/classes/hosts_spec.rb
@@ -3,34 +3,38 @@ require 'spec_helper'
 
 describe 'hosts' do
 
-	context 'Running on Debian' do
-		let(:facts) { { 
-			:operatingsystem => 'Debian'
-		} }
-		it { should include_class('hosts::params') }
-		it {
-			should contain_file('/etc/hosts').with( {
-			'ensure' => 'present',
-			'owner'  => 'root',
-			'group'  => 'root',
-			'mode'   => '0644'
-			} )
-		}
-	end
-	
-	context 'Running on Redhat' do
-		let(:facts) { { 
-			:operatingsystem => 'Redhat'
-		} }
-		it { should include_class('hosts::params') }
-		it {
-			should contain_file('/etc/hosts').with( {
-			'ensure' => 'present',
-			'owner'  => 'root',
-			'group'  => 'root',
-			'mode'   => '0644'
-			} )
-		}
+	[ 'Debian', 'Redhat' ].each do |os|
+		context "Running on #{os}" do
+			let(:facts) { { 
+				:operatingsystem => os
+			} }
+			let (:params) { {
+				:entries => [ '1.2.3.4 host.example.com host' ]
+			} }
+			it { should include_class('hosts::params') }
+			it {
+				should contain_file('/etc/hosts').with( {
+				'ensure' => 'present',
+				'owner'  => 'root',
+				'group'  => 'root',
+				'mode'   => '0644'
+				} )
+			}
+			
+			it 'contains definitions' do
+				should contain_file('/etc/hosts').with_content(
+					/^1\.2\.3\.4 host\.example\.com host/
+				)
+			end
+			context 'with motd enabled (enable_motd => true)' do
+				let(:params) { { :enable_motd => true } }
+				it { should contain_motd__register('Hosts')}
+			end
+			context 'with motd disabled (enable_motd => false)' do
+				let(:params) { { :enable_motd => false } }
+				it { should_not contain_motd__register('Hosts')}
+			end
+		end
 	end
 	
 	context 'Running on unsupported OS' do
@@ -41,24 +45,6 @@ describe 'hosts' do
 			expect { 
 				should include_class('hosts::params') 
 			}.to raise_error(Puppet::Error, /^Operating system.*/)
-		end
-	end
-	
-	context 'Without hosts definitions' do
-		let (:params) { {
-			:entries => nil
-		} }
-	end
-
-	context 'With hosts definitions' do
-		let (:facts)  { { :operatingsystem => 'Debian' } }
-		let (:params) { {
-			:entries => [ '1.2.3.4 host.example.com host' ]
-		} }
-		it do
-			should contain_file('/etc/hosts').with_content(
-				/^1\.2\.3\.4 host\.example\.com host/
-			)
 		end
 	end
 end
